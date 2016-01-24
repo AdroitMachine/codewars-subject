5c1133a5a76742f8dc515b780bc87128f63cc60f
Krew <wanhao.huang@outlook.com>
2016-01-24 11:13:36 +0800

    update

---
+++ Even or Odd.js
@@ -0,0 +1,15 @@
+// Description:
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+// top vote solution
+function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
+}
+
+
+//mine
+function even_or_odd(number) {
+  return number%2 ? 'Odd' : 'Even'
+}
