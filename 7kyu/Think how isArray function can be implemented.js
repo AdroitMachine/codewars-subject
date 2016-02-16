899f7b936932a2ed3f068b47c4c4ed7bee5b7eb8
Krew <wanhao.huang@outlook.com>
2016-02-16 20:51:59 +0800

    update

---
+++ 7kyu/Think how isArray function can be implemented.js
@@ -0,0 +1,26 @@
+// Description:
+
+// You know Array.isArray was already implement in ECMAScript 5.1 (ECMA-262). So this will be the polyfill.
+
+// Use Array.isArray to define method.
+
+// For example:
+
+// Array.isArray( [1,2,3] );  // true
+// Array.isArray( new Array() ); // true
+
+// Array.isArray( undefined ); // false
+// Array.isArray(17); // false
+
+
+// mine
+Array.isArray = function(value) {
+  return Object.prototype.toString.call(value) == '[object Array]'
+};
+
+
+
+// top vote solution
+Array.isArray = function(value) {
+  return Object.prototype.toString.call(value) === '[object Array]';
+};
